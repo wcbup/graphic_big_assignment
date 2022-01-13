7aff68d0084f1b2a8ef180482dd22b118769dfba
wcbup <wcbup123@foxmail.com>
2022-01-13 18:20:49 +0800

    tutorial 10

--- new/main.cpp
+++ new/main.cpp
@@ -11,7 +11,9 @@ using namespace std;
 using namespace glm;
 
 GLuint VBO;
+GLuint IBO;
 GLint gPositionLocation;
+GLint gColorLocation;
 GLuint gScaleLocation;
 GLuint gTranslationLocation;
 GLuint gRotationLocation;
@@ -28,7 +30,7 @@ void renderScene()
 	//{
 	//	delta *= -1;
 	//}
-	mat4 transplate(1, 0, 0, 1,
+	mat4 transplate(1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		0, 0, 0, 1);
@@ -40,35 +42,121 @@ void renderScene()
 		0, 0.2, 0, 0,
 		0, 0, 0.2, 0,
 		0, 0, 0, 1);
-	mat4 world = scale * transplate * rotate;
+	//mat4 world = scale * transplate * rotate;
 	//mat4 world = scale * rotate * transplate;
 	//mat4 world = rotate * transplate * scale;
+	mat4 world = transplate;
 
 	//bind the uniform variable
 	glUniformMatrix4fv(gTranslationLocation, 1, GL_TRUE, &world[0][0]);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+	//position
 	glEnableVertexAttribArray(gPositionLocation);
-	glVertexAttribPointer(gPositionLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glVertexAttribPointer(gPositionLocation, 3, GL_FLOAT, GL_FALSE,
+		6*sizeof(float), 0);
+
+	//color
+	glEnableVertexAttribArray(gColorLocation);
+	glVertexAttribPointer(gColorLocation, 3, GL_FLOAT, GL_FALSE,
+		6*sizeof(float), (void *)(sizeof(float)));
+
+	glDrawElements(GL_TRIANGLES, 54, GL_UNSIGNED_INT, 0);
+
 	glDisableVertexAttribArray(gPositionLocation);
+	glDisableVertexAttribArray(gColorLocation);
 
 	glutPostRedisplay();
 
 	glutSwapBuffers();
 }
 
-void createBuffer()
+struct Vertex {
+	vec3 pos;
+	vec3 color;
+
+	Vertex() {}
+
+	Vertex(float x, float y)
+	{
+		pos = vec3(x, y, 0.0f);
+
+		float red = (float)rand() / (float)RAND_MAX;
+		float green = (float)rand() / (float)RAND_MAX;
+		float blue = (float)rand() / (float)RAND_MAX;
+		color = vec3(red, green, blue);
+	}
+};
+
+void createVertexBuffer()
 {
-	glm::vec3 vertices[3];
-	vertices[0] = glm::vec3(-1, -1, 0);
-	vertices[1] = glm::vec3(1, -1, 0);
-	vertices[2] = glm::vec3(0, 1, 0);
+	Vertex Vertices[19];
+
+	// Center
+	Vertices[0] = Vertex(0.0f, 0.0);
+
+	// Top row
+	Vertices[1] = Vertex(-1.0f, 1.0f);
+	Vertices[2] = Vertex(-0.75f, 1.0f);
+	Vertices[3] = Vertex(-0.50f, 1.0f);
+	Vertices[4] = Vertex(-0.25f, 1.0f);
+	Vertices[5] = Vertex(-0.0f, 1.0f);
+	Vertices[6] = Vertex(0.25f, 1.0f);
+	Vertices[7] = Vertex(0.50f, 1.0f);
+	Vertices[8] = Vertex(0.75f, 1.0f);
+	Vertices[9] = Vertex(1.0f, 1.0f);
+
+	// Bottom row
+	Vertices[10] = Vertex(-1.0f, -1.0f);
+	Vertices[11] = Vertex(-0.75f, -1.0f);
+	Vertices[12] = Vertex(-0.50f, -1.0f);
+	Vertices[13] = Vertex(-0.25f, -1.0f);
+	Vertices[14] = Vertex(-0.0f, -1.0f);
+	Vertices[15] = Vertex(0.25f, -1.0f);
+	Vertices[16] = Vertex(0.50f, -1.0f);
+	Vertices[17] = Vertex(0.75f, -1.0f);
+	Vertices[18] = Vertex(1.0f, -1.0f);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices,
 		GL_STATIC_DRAW);
+}
+
+void createIndexBuffer()
+{
+	unsigned int Indices[] = { // Top triangles
+							   0, 2, 1,
+							   0, 3, 2,
+							   0, 4, 3,
+							   0, 5, 4,
+							   0, 6, 5,
+							   0, 7, 6,
+							   0, 8, 7,
+							   0, 9, 8,
+
+							   // Bottom triangles
+							   0, 10, 11,
+							   0, 11, 12,
+							   0, 12, 13,
+							   0, 13, 14,
+							   0, 14, 15,
+							   0, 15, 16,
+							   0, 16, 17,
+							   0, 17, 18,
+
+							   // Left triangle
+							   0, 1, 10,
+
+							   // Right triangle
+							   0, 18, 9 };
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices),
+		Indices, GL_STATIC_DRAW);
 }
 
 void addShader(GLuint shaderProgram, const char* pShaderText, GLenum shaderType)
@@ -155,6 +243,13 @@ void compileShader()
 		printf("Error getting attri location of 'Postion'\n");
 		exit(1);
 	}
+	gColorLocation = glGetAttribLocation(shaderProgram, "inColor");
+	//printf("gPositionLocation is %d\n", gPositionLocation);
+	if (gColorLocation == -1)
+	{
+		printf("Error getting attri location of 'inColor'\n");
+		exit(1);
+	}
 
 	//gScaleLocation = glGetUniformLocation(shaderProgram, "gScale");
 	//if (gScaleLocation == -1)
@@ -198,6 +293,8 @@ void compileShader()
 
 int main(int argc, char** argv)
 {
+	srand(GetCurrentProcessId());
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 
@@ -221,7 +318,8 @@ int main(int argc, char** argv)
 	GLclampf red = 0, green = 0, blue = 0, alpha = 0;
 	glClearColor(red, green, blue, alpha);
 
-	createBuffer();
+	createVertexBuffer();
+	createIndexBuffer();
 
 	compileShader();
 
