d9fd5160e36a3d899f95426365dadd53ad639f7d
wcbup <wcbup123@foxmail.com>
2022-01-18 03:21:55 +0800

    add sobel(lab7)

--- new/shader.fs
+++ new/shader.fs
@@ -23,6 +23,46 @@ uniform DirectionalLight gDirectionalLight;
 uniform Material gMaterial;
 uniform sampler2D gSampler;
 
+float getGrayScale(vec4 color)
+{
+    return color.x * 0.299f + color.y * 0.587f + color.z * 0.114f;
+}
+
+vec4 getColor(vec2 position)
+{
+    return texture2D(gSampler, position);
+}
+
+float getSobelGray(mat3 operator)
+{
+    float offset = 0.0001f;
+    float grayScale = 0;
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            grayScale += 
+                operator[i][j] * getGrayScale(getColor(vec2(TexCoord0.x + (i-1)*offset, TexCoord0.y + (j-1)*offset)));
+        }
+    }
+    return grayScale;
+}
+
+vec4 getSobelColor()
+{
+    float totalGray = 0.0f;
+    mat3 Gx = mat3(-1.0f, 0.0f, 1.0f,
+            -2.0f, 0.0f, 2.0f,
+            -1.0f, 0.0f, 1.0f);
+    mat3 Gy = mat3(1.0f, 2.0f, 1.0f,
+            0.0f, 0.0f, 0.0f,
+            -1.0f, -2.0f, -1.0f);
+    float gxScale = getSobelGray(Gx);
+    float gyScale = getSobelGray(Gy);
+    totalGray = sqrt(gxScale*gxScale + gyScale*gyScale);
+    return vec4(totalGray, totalGray, totalGray, 1.0f);
+}
+
 void main()
 {
     vec4 AmbientColor = vec4(gDirectionalLight.Color, 1.0f) *
@@ -42,4 +82,6 @@ void main()
 
     FragColor = texture2D(gSampler, TexCoord0.xy) *
                 (AmbientColor + DiffuseColor);
+
+    // FragColor = getSobelColor();
 }
